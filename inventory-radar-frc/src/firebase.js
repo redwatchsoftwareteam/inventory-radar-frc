b68766135670435b264baad28acac5f7720a17dd
NeonGems65 <119536884+NeonGems65@users.noreply.github.com>
2023-11-30 08:04:05 -0500

    Setup

---
+++ inventory-radar-frc/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvQHXabYkfhdMMqQzmcKa7AOzVJ8tu4qM",
+  authDomain: "inventory-radar-frc.firebaseapp.com",
+  projectId: "inventory-radar-frc",
+  storageBucket: "inventory-radar-frc.appspot.com",
+  messagingSenderId: "640999917257",
+  appId: "1:640999917257:web:042c4f39c26f3d13c0af18",
+  measurementId: "G-H4ZXY0PQ00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
