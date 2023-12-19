4207be959d00360930b4cd0c11dd0a5eb6b5846a
AkshayM1 <94143580+RVaman@users.noreply.github.com>
2023-12-19 17:23:36 -0500

    init

--- inventory-radar-frc/src/pages/App.jsx
+++ inventory-radar-frc/src/pages/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Page1 from './Page1';
+import Home from './Home';
+import '../index.css'
+import {Route, Routes} from "react-router-dom"
 
-const App = () => {
-
+function App () {
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/page1' element={<Page1 />}/>
+      </Routes>
     </div>
   )
 }
---
+++ inventory-radar-frc/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='body'>
+      <h1>Hello</h1>
+      <div>
+        <div>
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+        <div>
+          <button>Button 3</button>
+          <button>Button 4</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default home
